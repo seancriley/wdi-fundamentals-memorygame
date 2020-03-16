29631c30cd1ceb2312041dcd6c492e3d036b78ea
Sean Riley <seancriley@earthlink.net>
2020-03-15 22:30:34 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,16 @@
-console.log("Up and running!");
 
-let cardOne= "queen";
-let cardTwo= "queen";
-let cardThree= "king";
-let cardFour= "king";
 
-console.log("User flipped " + cardOne);
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardsInPlay.push (cardOne);
+cardsInPlay.push (cardTwo);
+
+console.log ("User flipped "+cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]) {alert("You found a match!");
+}else {alert("Sorry, try again.");
+}
+}
