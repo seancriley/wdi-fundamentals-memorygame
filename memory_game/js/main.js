fa624f22ffe5376f7d9146c1cd63fffb162a6e51
Sean Riley <seancriley@earthlink.net>
2020-03-15 23:38:03 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,15 +2,24 @@
 
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
-cardsInPlay.push (cardOne);
-cardsInPlay.push (cardTwo);
 
-console.log ("User flipped "+cardsInPlay);
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
+function flipCard(cardId) {
+console.log ("User flipped " + cards[cardId]);
+cardsInPlay.push (cards[cardId]);
 if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0]===cardsInPlay[1]) {alert("You found a match!");
-}else {alert("Sorry, try again.");
+	checkForMatch();
 }
 }
+
+
+
+flipCard (0);
+flipCard (2);
